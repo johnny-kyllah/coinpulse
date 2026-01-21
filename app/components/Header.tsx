ccb60cf8a5a4a6871c7256bbcfb2c9d84254236d
johnny kyllah <johnnykyllah@gmail.com>
2026-01-21 19:08:35 -0400

    implemented navigation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
                     <Link href="/" className={cn("nav-link", { "is-active": pathname === "/", "is-home": true })} >
                         Home
                     </Link>
-                    <p>Search Modal</p>
+                    <p> Search Modal</p>
                     <Link href="/coins" className={cn("nav-link", { "is-active": pathname === "/coins", })}>All Coins</Link>
                 </nav>
             </div>
